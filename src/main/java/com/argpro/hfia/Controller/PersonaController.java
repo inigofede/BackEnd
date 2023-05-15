7c5f48fd4ffd7c1c2d4fa90043062941abd0f98b
inigofede <fede.reveck@gmail.com>
2023-05-15 05:36:04 -0300

    CORS

--- src/main/java/com/argpro/hfia/Controller/PersonaController.java
+++ src/main/java/com/argpro/hfia/Controller/PersonaController.java
@@ -24,7 +24,7 @@ import org.springframework.web.bind.annotation.RestController;
  * @author Reveck
  */
 @RestController
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = "https://hfia-frontend.web.app")
 public class PersonaController {
     @Autowired IPersonaService ipersonaService;
     
--- src/main/java/com/argpro/hfia/Security/Controller/AuthController.java
+++ src/main/java/com/argpro/hfia/Security/Controller/AuthController.java
@@ -38,7 +38,7 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 @RequestMapping("/auth")
-@CrossOrigin(origins = {"http://localhost:4200"})
+@CrossOrigin(origins = {"https://hfia-frontend.web.app"})
 public class AuthController {
     @Autowired
     PasswordEncoder passwordEncoder;
